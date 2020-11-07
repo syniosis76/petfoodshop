045ed25e52fc841d18d24490ef86d7ae809a6f6f
SantaSammy <73153571+SantaSammy@users.noreply.github.com>
2020-11-08 12:24:47 +1300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "pl5olm9uaqn9xhli";
+$password = "o7mk03t99ecbrw8c";
+$dbname = "j4hxbdarc69h9toz";
+
+$con = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
