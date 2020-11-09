904decdf41bee6fc6032508dbf558a072637b824
SantaSammy <73153571+SantaSammy@users.noreply.github.com>
2020-11-10 11:09:51 +1300

    Add files via upload

---
+++ Sam Verner Dynamic/libs/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "k2fqe1if4c7uowsh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "d5iqslr5d9xtf5e7";
+$password = "nce2zlolndscddyi";
+$dbname = "lfp4a5owsypo4d6o";
+
+$con = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$con) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
