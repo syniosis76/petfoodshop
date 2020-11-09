ccfa89259cfddcc51e1feab6f21638ff62d593db
SantaSammy <73153571+SantaSammy@users.noreply.github.com>
2020-11-10 12:01:05 +1300

    Add files via upload

--- Sam Verner Dynamic/libs/config.php
+++ Sam Verner Dynamic/libs/config.php
@@ -11,7 +11,7 @@ if (!$con) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-$test1 = "SELECT title FROM Product WHERE id = 1";
+$test1 = "SELECT * FROM Product WHERE id = 1";
 $test1r = mysqli_query($con, $test1);
 $test1a = mysqli_fetch_assoc($test1r);
 
